7a5846595af632cb82bf1e71a7fdff1c867cf020
Manavvora <67179639+Manavvora@users.noreply.github.com>
2025-02-07 20:04:09 +0000

    Deploying to gh-pages from @ Manavvora/Manavvora.github.io@fddadeeaca786f1008ddd40d60b67aa615cb9774 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,7 +492,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%6B%76%6F%72%61%32@%69%6C%6C%69%6E%6F%69%73.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -513,14 +513,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=hr48LoAAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://manavvora.github.io/", "_blank");
         },
       },{
       id: 'light-theme',
